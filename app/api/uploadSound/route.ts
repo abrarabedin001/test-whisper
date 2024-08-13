ada26aff2ef1aaea52d272e81029d7192d16c76d
abrarabedin001 <abrarabedin001@gmail.com>
2024-08-14 02:14:29 +0600

    .

--- app/api/uploadSound/route.ts
+++ app/api/uploadSound/route.ts
@@ -1,3 +1,4 @@
+import { uploadFile2 } from '@/lib/firebase/firestore/firebaseSDK';
 import { NextRequest, NextResponse } from 'next/server';
 
 
@@ -8,9 +9,14 @@ export const config = {
 };
 
 export async function POST(request: NextRequest) {
- 
-  const req = await request.formData() as any; // TypeScript workaround to access the raw request
-  console.log('req', req);
+
+  const formData = await request.formData() as any; // TypeScript workaround to access the raw request
+  const file = formData.get('file') as File;
+
+  https://firebasestorage.googleapis.com/v0/b/transcription-d9c70.appspot.com/o/some-child?alt=media&token=ec8035a9-1a15-42c8-ad57-3d4d090130f4
+
+  await uploadFile2(file);
   return NextResponse.json({ status: 'received' }, { status: 200 });
+
 
 }
